a5f04606a7df5b51bee35286c02e58c8528dec59
Steve Wendt <stevewendt@cox.net>
2020-08-15 08:57:41 -0700

    Continuing to add photos and galleries

--- assets/js/photo_handler.js
+++ assets/js/photo_handler.js
@@ -1,16 +1,51 @@
 
-let photoDiv = $('.item-photo');
+// let photoDiv = $('.item-photo');
+//
+// $(document).ready(function() {
+//     setPhotoDimensions(photoDiv);
+// });
+//
+// $(window).resize(function(e) {
+//     setPhotoDimensions(photoDiv);
+// });
+//
+// function setPhotoDimensions(photoDiv) {
+//     let photoWidth = photoDiv.parent().width() - 10;
+//     let photoHeight = photoWidth * .75;
+//     photoDiv.css({'width': photoWidth, 'height': photoHeight});
+// }
 
-$(document).ready(function() {
-    setPhotoDimensions(photoDiv);
+
+let cardImage = $(".card-img-top img");
+
+cardImage.mouseenter(function() {
+
+  if(this.clientWidth === 225) {
+    $(this).css({
+      "height": "400px",
+      "width": "300px"
+    });
+  } else {
+    alert("in the else");
+    $(this).css({
+      "height": "300px",
+      "width": "400px"
+    });
+  }
+
 });
 
-$(window).resize(function(e) {
-    setPhotoDimensions(photoDiv);
+cardImage.mouseleave(function() {
+  // alert(this.clientWidth);
+  if(this.clientWidth === 300) {
+    $(this).css({
+      "height": "300px",
+      "width": "225px"
+    });
+  } else {
+    $(this).css({
+      "height": "225px",
+      "width": "300px"
+    });
+  }
 });
-
-function setPhotoDimensions(photoDiv) {
-    let photoWidth = photoDiv.parent().width() - 10;
-    let photoHeight = photoWidth * .75;
-    photoDiv.css({'width': photoWidth, 'height': photoHeight});
-}
