2f5b5a6ec6435724e5a4e19e8c4d645df3c7dba6
Steve Wendt <stevewendt@cox.net>
2020-09-19 08:59:36 -0700

    Added adult warning as well as a few more items in the galleries

---
+++ assets/js/warning.js
@@ -0,0 +1,26 @@
+//Check to see if user has accepted the warning before
+$(document).ready(function() {
+  if ( localStorage.getItem('accepted') === null) {
+    // $("#myModal").modal('show');
+    $('.modal').appendTo("body").modal('show');
+  }
+  else {
+    removeBackdrop();
+  }
+});
+
+//When user clicks the enter button, add token to localStorage
+$("#enter-btn").click(function () {
+  localStorage.setItem('accepted', 'true')
+  removeBackdrop();
+})
+
+$("#leave-btn").click(function () {
+  window.location = "https://google.com";
+})
+
+function removeBackdrop() {
+  $('#warningModal').modal('hide');
+  $('body').removeClass('modal-open');
+  $('.modal-backdrop').remove();
+}
